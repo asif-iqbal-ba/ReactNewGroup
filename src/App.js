f1f268904b70c59e25c77e6d48f3ed10f62de906
asif-iqbal-ba <asifssiabal@gmail.com>
2022-09-11 20:57:39 +0500

    states and functions

--- src/App.js
+++ src/App.js
@@ -5,62 +5,82 @@ import BasicTextFields from './Components/Textfeilds.js'
 let fName ='Abdullah'
 
 class Header extends React.Component{
+  constructor(){
+    super()
+    this.state ={
+      name:"Asif Iqbal",
+      email:"asif@gmail.com"
+    }
+  }
+
+  setName =()=>{
+    console.log('Running',this.state.name)
+
+  }
   render() {
     return (
+      <div>
+        <h2>
+          {this.state.name}
+        </h2>
+        <input type="text" placeholder='Enter Name Here' />
+        <button onClick={this.setName}>Change Name</button>
+      </div>
       // <div>
       //   <h2 style={{color:"red"}}>Header Component</h2>
       //   <button type="button" className="btn btn-danger">Danger</button>
       // </div>
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      //   <div className="container-fluid">
+      //     <a className="navbar-brand" href="#">Navbar</a>
+      //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      //       <span className="navbar-toggler-icon" />
+      //     </button>
+      //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      //       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      //         <li className="nav-item">
+      //           <a className="nav-link active" aria-current="page" href="#">Home</a>
+      //         </li>
+      //         <li className="nav-item">
+      //           <a className="nav-link" href="#">Link</a>
+      //         </li>
+      //         <li className="nav-item dropdown">
+      //           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      //             Dropdown
+      //           </a>
+      //           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+      //             <li><a className="dropdown-item" href="#">Action</a></li>
+      //             <li><a className="dropdown-item" href="#">Another action</a></li>
+      //             <li><hr className="dropdown-divider" /></li>
+      //             <li><a className="dropdown-item" href="#">Something else here</a></li>
+      //           </ul>
+      //         </li>
+      //         <li className="nav-item">
+      //           <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
+      //         </li>
+      //       </ul>
+      //       <form className="d-flex">
+      //         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+      //         <button className="btn btn-outline-success" type="submit">Search</button>
+      //       </form>
+      //     </div>
+      //   </div>
+      // </nav>
     );
   }
 }
 
 class App extends React.Component{
   render(){
-    let myName = 'Asif'
-    let arr = ['a','b','c']
-    let todoes =[{id:1,Name:"Asif"},{id:2,Name:"Iqbal"}]
+    // let myName = 'Asif'
+    // let arr = ['a','b','c']
+    // let todoes =[{id:1,Name:"Asif"},{id:2,Name:"Iqbal"}]
     return(
       <div>
         <Header/>
-        <h1>
+
+        {/* <h1>
           Hello World {myName} {fName} {arr}
         </h1>
 
@@ -69,7 +89,7 @@ class App extends React.Component{
             return <li key={index}>{`${item.id} - ${item.Name}`} </li>
           })}
         </ul>
-        <BasicTextFields />
+        <BasicTextFields /> */}
       </div>
     )
   }
